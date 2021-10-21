27de6b0260ab9e36d94e2a42466c50188133468d
plr-65 <rolapa65@hotmail.com>
2021-10-21 10:33:48 +0200

    Agregar archivos de proyecto.

---
+++ Client/Clases/Botones.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Peliculas.Client.Clases
+{
+    public class Botones
+    {
+        public static void presionarboton()
+        {
+            Console.WriteLine("Oleee");
+        }
+    }
+}
---
+++ Shared/Entidades/Peliculas.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Peliculas.Shared.Entidades
+{
+    public class Pelicula
+    {
+        public string Titulo { get; set; }
+        public DateTime Lanzamiento { get; set; }
+
+    }
+}
